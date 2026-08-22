bc6084afcba490455ccfd1b6f648eac0c4c3b8a3
Mahmoud Zuhair Mahmoud Abu Hilal <mahmoedzohirm@gmail.com>
2026-08-22 19:51:46 +0200

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,7 @@
+// لا تستورد dotenv
 import { neon } from '@neondatabase/serverless';
-import dotenv from 'dotenv';
-dotenv.config();
-const connectionString = process.env.DATABASE_URL;
-if (!connectionString) throw new Error('❌ DATABASE_URL غير موجود');
+
+// استخدم import.meta.env (Vite)
+const connectionString = import.meta.env.VITE_DATABASE_URL;
+if (!connectionString) throw new Error('❌ DATABASE_URL غير موجود في .env');
 export const sql = neon(connectionString);
